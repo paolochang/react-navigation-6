0e0f22b3e1b4d798fb6c1918b4d848a290ec37dd
Paolo Chang <chanhwan.chang@gmail.com>
2022-09-11 00:44:59 -0400

    RN: main screen

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+// Configuration: react-native-svg-transformer for React Native v0.59 or newer
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
+  };
+})();
